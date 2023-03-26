a173807eea9090649edfc0c68143cb6b40727335
nbarnabee <nicole.barnabee@gmail.com>
2023-03-26 16:19:57 +0200

    Convert function list to generic CardMaker

--- recipes/main.js
+++ recipes/main.js
@@ -1,64 +1,73 @@
-const recipes = {
-  "Generic Recipe 1": {
-    ingredients: {
-      milk: [1, "cup"],
-      flour: [1, "cup"],
-      sugar: [1, "cup"],
-      eggs: [2, ""],
-    },
-    instructions: ["Put in a bowl.", "Mix around.", "Pour onto ground."],
-  },
-  "Generic Recipe 2": {
-    ingredients: {
-      butter: [1, "TB"],
-      flour: [1, "cup"],
-      sugar: [1, "cup"],
-      eggs: [2, ""],
-    },
-    instructions: ["Put in a bowl.", "Mix around.", "Pour onto ground."],
-  },
-  "Generic Recipe 3": {
-    ingredients: {
-      milk: [1, "cup"],
-    },
-    instructions: ["Pour into glass.", "Drink."],
-  },
-};
+/* 
+This is a generic card constructor that accepts a path to the file 
+where the data that's to be rendered is stored.
 
-/* ---- SELECT MENU ------ */
+The way the card is constructed does require a specific structure to 
+the JSON data, which makes it, perhaps, not very useful to anyone other 
+than me, but it's an interesting exercise.
 
-const selectMenu = document.getElementById("select");
+*/
 
-for (let recipe in recipes) {
-  const option = document.createElement("option");
-  option.value = recipe;
-  option.textContent = recipe;
-  option.addEventListener("click", makeCard);
-  selectMenu.appendChild(option);
-}
-
-/* ---- CARD MAKER ------ */
-
-function makeCard(e) {
-  document.getElementById("title").innerText = e.target.value;
-  const ingredients = document.getElementById("ingredients");
-  const instructions = document.getElementById("instructions");
-  ingredients.innerHTML = ""; // clear first
-  instructions.innerHTML = "";
-
-  const recipe = recipes[e.target.value];
-  const ingredientList = recipe.ingredients; // an object
-  const instructionList = recipe.instructions; // an array
-
-  for (entry in ingredientList) {
-    const ingredient = document.createElement("li");
-    ingredient.textContent = `${ingredientList[entry][0]} ${ingredientList[entry][1]} ${entry}`;
-    ingredients.appendChild(ingredient);
+class CardMaker {
+  constructor(dataPath) {
+    this.dataPath = dataPath;
   }
+  async makeSelect() {
+    await this.fetchData();
+    this.populateMenu();
+  }
+  async fetchData() {
+    let response = await fetch(this.dataPath);
+    const data = await response.json();
+    this.data = data;
+  }
+  populateMenu() {
+    this.setOption("generic");
+    for (let item in this.data.content) {
+      this.setOption(item);
+    }
+  }
+  setOption(item) {
+    const selectMenu = document.getElementById("select");
+    const option = document.createElement("option");
+    if (item == "generic") {
+      option.value = "";
+      option.setAttribute("disabled", true);
+      option.setAttribute("selected", true);
+      option.setAttribute("hidden", true);
+      option.textContent = `--- ${this.data.title} ---`;
+    } else {
+      option.value = item;
+      option.textContent = item;
+      option.addEventListener("click", this.makeCard.bind(this));
+    }
+    selectMenu.appendChild(option);
+  }
+  makeCard(e) {
+    document.getElementById("title").innerText = e.target.value;
+    const ingredients = document.getElementById("ingredients");
+    const instructions = document.getElementById("instructions");
+    ingredients.innerHTML = ""; // clear first
+    instructions.innerHTML = "";
 
-  instructionList.forEach((entry) => {
-    const instruction = document.createElement("li");
-    instruction.textContent = entry;
-    instructions.appendChild(instruction);
-  });
+    const recipe = this.data.content[e.target.value];
+    const ingredientList = recipe.ingredients; // an object
+    const instructionList = recipe.instructions; // an array
+
+    for (let entry in ingredientList) {
+      const ingredient = document.createElement("li");
+      ingredient.textContent = `${ingredientList[entry].join(" ")} ${entry}`;
+      ingredients.appendChild(ingredient);
+    }
+
+    instructionList.forEach((entry) => {
+      const instruction = document.createElement("li");
+      instruction.textContent = entry;
+      instructions.appendChild(instruction);
+    });
+  }
 }
+
+const path = "./data/recipes.json";
+const recipeMaker = new CardMaker(path);
+recipeMaker.makeSelect();
